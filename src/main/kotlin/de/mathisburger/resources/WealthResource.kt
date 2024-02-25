ee1c4c2e49fcaab674d81759fb4103db19f9d103
Mathis Burger <handymathis@gmail.com>
2024-02-25 16:02:26 +0100

    [TASK] Added wealth calculation

--- src/main/kotlin/de/mathisburger/resources/WealthResource.kt
+++ src/main/kotlin/de/mathisburger/resources/WealthResource.kt
@@ -1,4 +1,33 @@
 package de.mathisburger.resources
 
+import de.mathisburger.service.WealthService
+import jakarta.inject.Inject
+import org.eclipse.microprofile.graphql.GraphQLApi
+import org.eclipse.microprofile.graphql.Query
+
+@GraphQLApi
 class WealthResource {
+
+    @Inject
+    lateinit var wealthService: WealthService;
+
+    @Query
+    fun getGrossWealthWithInflation(): Long {
+        return this.wealthService.getGrossWealthWithInflation();
+    }
+
+    @Query
+    fun getGrossWealthWithoutInflation(): Long {
+        return this.wealthService.getGrossWealthWithoutInflation();
+    }
+
+    @Query
+    fun getNetWealthWithInflation(): Long {
+        return this.wealthService.getNetWealthWithInflation();
+    }
+
+    @Query
+    fun getNetWealthWithoutInflation(): Long {
+        return this.wealthService.getNetWealthWithoutInflation();
+    }
 }
--- src/main/kotlin/de/mathisburger/service/WealthService.kt
+++ src/main/kotlin/de/mathisburger/service/WealthService.kt
@@ -4,6 +4,8 @@ import de.mathisburger.repository.HousePriceChangeRepository
 import de.mathisburger.repository.RealEstateRepository
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 @ApplicationScoped
 class WealthService {
@@ -14,12 +16,77 @@ class WealthService {
     @Inject
     lateinit var housePriceChangeRepository: HousePriceChangeRepository;
 
-    fun getGrossWealth(): Long {
+    fun getGrossWealthWithInflation(): Long {
+        val objects = this.realEstateRepository.listAll();
+        var wealth = 0L;
+        for (obj in objects) {
+            val priceChanges = this.housePriceChangeRepository.findByZip(obj.zip!!);
+            var value = obj.initialValue!!;
+            val calendar = GregorianCalendar();
+            calendar.time = obj.dateBought!!;
+            val startYear = calendar.get(Calendar.YEAR);
+            for (priceChange in priceChanges) {
+                if (priceChange.year!! > startYear) {
+                    val multi = 1 + (priceChange.change!! / 100);
+                    value = (value.toDouble() * multi).toLong();
+                }
+            }
+            wealth += value;
+        }
+        return wealth;
+    }
+
+    fun getGrossWealthWithoutInflation(): Long {
         val objects = this.realEstateRepository.listAll();
         var wealth = 0L;
         for (obj in objects) {
             wealth += obj.initialValue!!;
         }
         return wealth;
+    }
+
+    fun getNetWealthWithInflation(): Long {
+        val objects = this.realEstateRepository.listAll();
+        var netWealth = 0L;
+        for (obj in objects) {
+            var cumulatedRate = 0.0;
+            for (rate in obj.credit!!.rates) {
+                cumulatedRate += rate.amount!!;
+            }
+            if (cumulatedRate.toLong() < obj.credit!!.amount!!) {
+                netWealth += cumulatedRate.toLong();
+                continue;
+            }
+            val priceChanges = this.housePriceChangeRepository.findByZip(obj.zip!!);
+            var value = obj.initialValue!!;
+            val calendar = GregorianCalendar();
+            calendar.time = obj.dateBought!!;
+            val startYear = calendar.get(Calendar.YEAR);
+            for (priceChange in priceChanges) {
+                if (priceChange.year!! > startYear) {
+                    val multi = 1 + (priceChange.change!! / 100);
+                    value = (value.toDouble() * multi).toLong();
+                }
+            }
+            netWealth += value;
+        }
+        return netWealth;
+    }
+
+    fun getNetWealthWithoutInflation(): Long {
+        val objects = this.realEstateRepository.listAll();
+        var netWealth = 0L;
+        for (obj in objects) {
+            var cumulatedRate = 0.0;
+            for (rate in obj.credit!!.rates) {
+                cumulatedRate += rate.amount!!;
+            }
+            if (cumulatedRate.toLong() < obj.credit!!.amount!!) {
+                netWealth += cumulatedRate.toLong();
+                continue;
+            }
+            netWealth += obj.initialValue!!;
+        }
+        return netWealth;
     }
 }
