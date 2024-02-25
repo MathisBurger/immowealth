2b60930d2e5e6ebd091935251650f516fa3d947d
Mathis Burger <handymathis@gmail.com>
2024-02-25 19:52:06 +0100

    [TASK] Added apollo support

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import {Box, CssBaseline, CssVarsProvider} from "@mui/joy";
-import PathDisplay from "@/components/PathDisplay";
-import Sidebar from "@/components/Sidebar";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import BaseLayout from "@/components/BaseLayout";
+import {ReactNode} from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,42 +11,22 @@ export const metadata: Metadata = {
   description: "Simple application to track your immo wealth",
 };
 
+const client = new ApolloClient({
+    uri: 'http://localhost:8080/',
+    cache: new InMemoryCache(),
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="de">
       <body className={inter.className}>
-      <CssVarsProvider disableTransitionOnChange>
-        <CssBaseline />
-        <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
-            <Sidebar />
-          <Box
-              component="main"
-              className="MainContent"
-              sx={{
-                px: { xs: 2, md: 6 },
-                pt: {
-                  xs: 'calc(12px + var(--Header-height))',
-                  sm: 'calc(12px + var(--Header-height))',
-                  md: 3,
-                },
-                pb: { xs: 2, sm: 2, md: 3 },
-                flex: 1,
-                display: 'flex',
-                flexDirection: 'column',
-                minWidth: 0,
-                height: '100dvh',
-                gap: 1,
-              }}
-          >
-            <PathDisplay />
-            {children}
-        </Box>
-        </Box>
-      </CssVarsProvider>
+        <ApolloProvider client={client}>
+            <BaseLayout children={children} />
+        </ApolloProvider>
       </body>
     </html>
   );
---
+++ web/components/BaseLayout.tsx
@@ -0,0 +1,41 @@
+import {Box, CssBaseline, CssVarsProvider} from "@mui/joy";
+import Sidebar from "@/components/Sidebar";
+import PathDisplay from "@/components/PathDisplay";
+import {ReactNode} from "react";
+
+interface BaseLayoutProps {
+    children: ReactNode;
+}
+
+const BaseLayout = ({children}: BaseLayoutProps) => (
+    <CssVarsProvider disableTransitionOnChange>
+        <CssBaseline />
+        <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+            <Sidebar />
+            <Box
+                component="main"
+                className="MainContent"
+                sx={{
+                    px: { xs: 2, md: 6 },
+                    pt: {
+                        xs: 'calc(12px + var(--Header-height))',
+                        sm: 'calc(12px + var(--Header-height))',
+                        md: 3,
+                    },
+                    pb: { xs: 2, sm: 2, md: 3 },
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minWidth: 0,
+                    height: '100dvh',
+                    gap: 1,
+                }}
+            >
+                <PathDisplay />
+                {children}
+            </Box>
+        </Box>
+    </CssVarsProvider>
+);
+
+export default BaseLayout;
