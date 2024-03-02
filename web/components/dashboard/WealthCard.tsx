b07245bcf2bc61aa413204c71539521c1b0f3d08
Mathis Burger <handymathis@gmail.com>
2024-03-02 17:42:09 +0100

    [BUGFIX] Fixed dashboard wealth display

--- web/components/dashboard/WealthCard.tsx
+++ web/components/dashboard/WealthCard.tsx
@@ -8,38 +8,43 @@ interface WealthCardProps {
     label: string;
 }
 
-const WealthCard = ({data, label}: WealthCardProps) => (
-    <Grid xs={6}>
-        <Card size="sm" variant="soft">
-            <Typography level="h4">
-                {label}
-            </Typography>
-            <Grid container direction="row" spacing={2}>
-                <Grid xs={6} alignItems="center">
-                    <Typography level="h2">
-                        {data?.total}€
-                    </Typography>
-                </Grid>
-                <Grid xs={6}>
-                    <PieChart
-                        legend={{hidden: true}}
-                        series={[
-                            {
-                                //@ts-ignore
-                                data: data?.detailed ?? [],
-                                innerRadius: 70,
-                                outerRadius: 100
-                            },
-                        ]}
-                        width={300}
-                        height={200}
-                        tooltip={{trigger: 'item'}}
-                    />
-                </Grid>
+const WealthCard = ({data, label}: WealthCardProps) => {
 
-            </Grid>
-        </Card>
-    </Grid>
-)
+    //console.log(data);
+
+    return (
+        <Grid xs={6}>
+            <Card size="sm" variant="soft">
+                <Typography level="h4">
+                    {label}
+                </Typography>
+                <Grid container direction="row" spacing={2}>
+                    <Grid xs={6} alignItems="center">
+                        <Typography level="h2">
+                            {data?.total}€
+                        </Typography>
+                    </Grid>
+                    <Grid xs={6}>
+                        <PieChart
+                            legend={{hidden: true}}
+                            series={[
+                                {
+                                    //@ts-ignore
+                                    data: data?.detailed ?? [],
+                                    innerRadius: 70,
+                                    outerRadius: 100
+                                },
+                            ]}
+                            width={300}
+                            height={200}
+                            tooltip={{trigger: 'item'}}
+                        />
+                    </Grid>
+
+                </Grid>
+            </Card>
+        </Grid>
+    );
+}
 
 export default WealthCard;
