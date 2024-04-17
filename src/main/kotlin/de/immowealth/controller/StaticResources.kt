2e3759f6bfdbf133d579c46dab1691a453e7a799
Mathis Burger <handymathis@gmail.com>
2024-04-17 18:02:40 +0200

    [TASK] Fixed serve of static files

--- src/main/kotlin/de/immowealth/controller/StaticResources.kt
+++ src/main/kotlin/de/immowealth/controller/StaticResources.kt
@@ -4,12 +4,23 @@ import io.quarkus.runtime.StartupEvent
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.StaticHandler
 import jakarta.enterprise.event.Observes
+import java.io.File
 
+/**
+ * Handles static resources
+ */
 class StaticResources {
 
     fun installRoutes(@Observes startupEvent: StartupEvent, router: Router) {
         router.route()
             .path("/*")
             .handler(StaticHandler.create("static/"))
+        router.errorHandler(404) { routingContext ->
+            var file = File("static/" + routingContext.request().path() + ".html")
+            if (!file.canRead()) {
+                file = File("static/404.html");
+            }
+            routingContext.response().setStatusCode(200).end(file.readText())
+        }
     }
 }
--- src/main/kotlin/de/immowealth/resources/HelloResource.kt
+++
@@ -1,2 +0,0 @@
-package de.immowealth.resources
-
