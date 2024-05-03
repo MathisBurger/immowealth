e4c187436262e2f6a2662c9c0688031354c7ce99
Mathis Burger <handymathis@gmail.com>
2024-05-03 12:18:36 +0200

    [TEST] Added tests for new tenant service functionality

--- src/test/kotlin/de/immowealth/service/TenantServiceTest.kt
+++ src/test/kotlin/de/immowealth/service/TenantServiceTest.kt
@@ -6,13 +6,17 @@ import jakarta.inject.Inject
 import jakarta.ws.rs.core.SecurityContext
 import org.eclipse.microprofile.jwt.JsonWebToken
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.opentest4j.AssertionFailedError
 
 /**
  * Tests the functionality of the tenant service
  */
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class TenantServiceTest : AbstractServiceTest() {
 
     @Inject
@@ -25,6 +29,7 @@ class TenantServiceTest : AbstractServiceTest() {
     lateinit var userService: UserService;
 
     @Test
+    @Order(1)
     fun testCreateTenantAsAdmin() {
         this.loginAsUser("admin");
         val ten = this.tenantService.createTenant("ten1", "usr", "pwd", "test@test.de");
@@ -32,6 +37,7 @@ class TenantServiceTest : AbstractServiceTest() {
     }
 
     @Test
+    @Order(2)
     fun testCreateTenantAsUnknownUser() {
         this.logout();
         try {
@@ -45,6 +51,7 @@ class TenantServiceTest : AbstractServiceTest() {
     }
 
     @Test
+    @Order(3)
     fun testCreateTenantAsNonAdmin() {
         this.loginAsUser("admin");
         this.userService.registerUser("nonAdmin", "123", "a@a.a", mutableListOf());
@@ -52,6 +59,64 @@ class TenantServiceTest : AbstractServiceTest() {
             this.loginAsUser("nonAdmin")
             this.tenantService.createTenant("ren1", "usr", "pwd", "test@test.de");
             fail<String>("Cannot create as non admin")
+        } catch (e: UnauthorizedException) {
+            assertTrue(true)
+        } catch (e: AssertionFailedError) {
+            throw e;
+        }
+    }
+
+    @Test
+    @Order(4)
+    fun testGetAllTenants() {
+        this.loginAsUser("admin");
+        val amount = this.tenantService.getAllTenants();
+        assertTrue(amount.isNotEmpty())
+    }
+
+    @Test
+    @Order(5)
+    fun testGetAllTenantsAsNonAdmin() {
+        this.loginAsUser("nonAdmin");
+        val tenants = this.tenantService.getAllTenants();
+        assertTrue(tenants.isEmpty())
+    }
+
+    @Test
+    @Order(6)
+    fun testGetAllTenantsAsOwner() {
+        this.loginAsUser("usr");
+        val tenants = this.tenantService.getAllTenants();
+        assertSame(1, tenants.size)
+    }
+
+    @Test
+    @Order(7)
+    fun testGetTenant() {
+        this.loginAsUser("admin");
+        val tenant = this.tenantService.getAllTenants().first();
+        val fetched = this.tenantService.getTenant(tenant.id!!);
+        assertSame(fetched.id, tenant.id)
+    }
+
+    @Test
+    @Order(8)
+    fun testGetTenantAsOwner() {
+        this.loginAsUser("usr");
+        val tenant = this.tenantService.getAllTenants().first();
+        val fetched = this.tenantService.getTenant(tenant.id!!);
+        assertSame(fetched.id, tenant.id)
+    }
+
+    @Test
+    @Order(9)
+    fun testGetTenantAsUnknownUser() {
+        this.loginAsUser("usr");
+        val tenant = this.tenantService.getAllTenants().first();
+        this.logout();
+        try {
+            this.tenantService.getTenant(tenant.id!!);
+            fail<String>("Should not be able to get tenant")
         } catch (e: UnauthorizedException) {
             assertTrue(true)
         } catch (e: AssertionFailedError) {
